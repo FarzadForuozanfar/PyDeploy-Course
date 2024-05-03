e4e01f663a787d1b1708934e066e58932d356312
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2024-05-03 20:48:18 +0330

    no message

--- Assignment06/University_System/scripts/database.py
+++ Assignment06/University_System/scripts/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://root:DB1234@localhost:5432/database_university" 
-# SQLALCHEMY_DATABASE_URL = "postgresql://root:veO80ulzrg30IWsMarRxnPVX@farzad-py-deployment-db:5432/postgres" 
-SQLALCHEMY_DATABASE_URL = "postgres://root:atuOw9iNHdEy5BcbBWZ74RmZLPfbW2fM@dpg-coqh9kdjm4es73an38kg-a/unidb_ymes" 
+# SQLALCHEMY_DATABASE_URL = "postgresql://root:muiBJrnwBJW36ZHPA8Gdi8bK@farzad-py-deployment-db:5432/postgres" 
+SQLALCHEMY_DATABASE_URL = "postgres://root:sywdtZIlCI3Q8QswjrZHCtjL0vz57jse@dpg-coqhmmtjm4es73anb4pg-a.oregon-postgres.render.com/unidb_s5zr" 
 
 
 engine = create_engine(
